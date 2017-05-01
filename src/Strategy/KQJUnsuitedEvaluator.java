80722fc846d2d6d8d10d874108b4a976e01ac38b
João asd <Joo asd>
2017-05-01 23:16:59 +0100

    Added a lot

--- src/Strategy/KQJUnsuitedEvaluator.java
+++ src/Strategy/KQJUnsuitedEvaluator.java
@@ -5,28 +5,34 @@ import videopoker.Hand;
 import videopoker.Card.Suit;
 import videopoker.Card.Value;
 
-public class AKQJUnsuited extends HandEvaluator implements Evaluator {
-	
-public boolean[] whereCards(Hand hand){
+public class KQJUnsuitedEvaluator extends HandEvaluator implements Evaluator {
+
+	public boolean[] whereCards(Hand hand){
 		
 		Card[] hand_o = hand.orderByValue();
 		int card_val = 11;
-		int not_card = 1;
+		int yes_card_idx = 0;
+		int[] yes_card = {0, 0, 0};
 		
 		for(int i = 0; i < 5; i++){
 			if(hand_o[i].getValueWeight() == card_val){
 				card_val++;
-			}else{
-				not_card = i;
+				yes_card[yes_card_idx] = i;
+				yes_card_idx++;
+			}
+			if(card_val == 14){
+				break;
 			}
 		}
 		
-		if(card_val != 15){
+		if(card_val != 14){
 			return new boolean[0];
 		}
 		
-		boolean[] keep = {true, true, true, true, true};
-		keep[hand.search(hand_o[not_card])] = false;
+		boolean[] keep = {false,false,false,false,false};
+		keep[hand.search(hand_o[yes_card[0]])] = true;
+		keep[hand.search(hand_o[yes_card[1]])] = true;
+		keep[hand.search(hand_o[yes_card[2]])] = true;
 
 		return keep;
 	}
@@ -40,7 +46,7 @@ public boolean[] whereCards(Hand hand){
 		Card c5 = new Card(Value.JACK, Suit.HEARTS);
 
 		Hand hand = new Hand(c1,c2,c3,c4,c5);
-		AKQJUnsuited eval = new AKQJUnsuited();
+		KQJUnsuitedEvaluator eval = new KQJUnsuitedEvaluator();
 		boolean[] keep = eval.whereCards(hand);
 		
 		for (int i = 0; i < keep.length; i++){
