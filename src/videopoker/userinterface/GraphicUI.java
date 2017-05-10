f967efbc8d1e46cf389925a0be2b740b98c532bb
Eduardo Gomes <eduardo.l.gomes@tecnico.ulisboa.pt>
2017-05-11 00:34:54 +0100

    Updated graphicUI

--- src/videopoker/userinterface/GraphicUI.java
+++ src/videopoker/userinterface/GraphicUI.java
@@ -9,11 +9,13 @@ import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.TextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.IOException;
+import java.text.NumberFormat;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
@@ -22,8 +24,10 @@ import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComponent;
+import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -36,6 +40,7 @@ import videopoker.utilities.PowerHashMap;
 
 public class GraphicUI extends JFrame implements UserInterface{
 	private Game mGame;
+	private JFrame creditWindow;
 	private boolean[] keepArray = {false,false,false,false,false};
 	private JLabel[] keepText = new JLabel[5];
 	private int mBet = 5;
@@ -46,6 +51,7 @@ public class GraphicUI extends JFrame implements UserInterface{
 	private JLabel alertMsgLabel;
 	private JPanel[] cardsOuterpannel = new JPanel[5];	
 	
+	private JLabel pCredit;
 	private JButton betButton, dealButton, drawButton, adviceButton, statsButton;
 	
 	private void cleanVars(){
@@ -64,14 +70,51 @@ public class GraphicUI extends JFrame implements UserInterface{
 	public GraphicUI(Game game) {
 		mGame = game;
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 800, 600);
-	   
+		setBounds(100, 100,910, 700);
+		
+		/*Credit input window*/
+		creditWindow = new JFrame();
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		creditWindow.setTitle("VideoPoker - Set your credit");
+		creditWindow.setContentPane(new JPanel());
+		creditWindow.setLayout(new FlowLayout());
+		
+		JLabel alertCredit = new JLabel("Please insert your starting credit");
+		creditWindow.add(alertCredit);
+		
+		TextField amountField = new TextField();
+		amountField.setPreferredSize(new Dimension(100, 20));
+		amountField.setMaximumSize(new Dimension(100, 20));
+		amountField.setMinimumSize(new Dimension(100, 20));
+		creditWindow.add(amountField);
+		
+		JButton submitCredit = new JButton("Start Game");
+		submitCredit.addActionListener(new ActionListener(){
+	      public void actionPerformed(ActionEvent e) {
+	    	  String strValue = (String) amountField.getText();
+	    	  try { 
+	    		  int intValue = Integer.parseInt(strValue);
+	    		  mGame.getPlayer().setCredit(intValue);
+	    		  setVisible(true);
+	    		  pCredit.setText("Credit: " + String.valueOf(mGame.getPlayer().getCredit()));
+	    		  creditWindow.setVisible(false);
+	    	  }
+	    	  catch (NumberFormatException e2) {
+	    		  JOptionPane.showMessageDialog(null, "Please insert a valid number");
+	    	  }
+	      }
+	    });
+		creditWindow.add(submitCredit);
+		creditWindow.pack();
+		creditWindow.setResizable(false);
+		
 		//SET BACKGROUND
 		Image bg = null;
 	    if( (bg = imageReader("bg.jpg")) == null){
 	    	throw new RuntimeException();
 	    }
-	    Image bgResized = bg.getScaledInstance(800, 600, Image.SCALE_DEFAULT);
+	    Image bgResized = bg.getScaledInstance(910, 700, Image.SCALE_DEFAULT);
 	    setContentPane(new JComponent() {
 	        @Override public void paintComponent(Graphics g) {
 	            g.drawImage(bgResized, 0, 0, null);
@@ -84,9 +127,9 @@ public class GraphicUI extends JFrame implements UserInterface{
 	    BoxLayout winningsLayout = new BoxLayout(winningsLabel,BoxLayout.Y_AXIS);
 	    winningsLabel.setLayout(winningsLayout);
 	    winningsLabel.setOpaque(false);
-	    winningsLabel.setPreferredSize(new Dimension(750, 300));
-	    winningsLabel.setMaximumSize(new Dimension(750, 300)); 
-	    winningsLabel.setMinimumSize(new Dimension(750, 300));
+	    winningsLabel.setPreferredSize(new Dimension(900, 300));
+	    winningsLabel.setMaximumSize(new Dimension(900, 300)); 
+	    winningsLabel.setMinimumSize(new Dimension(900, 300));
 	    add(winningsLabel,BorderLayout.NORTH);
 	    
 	    int it = 0;
@@ -96,17 +139,17 @@ public class GraphicUI extends JFrame implements UserInterface{
     	JPanel betsPanel = new JPanel();
     	BoxLayout betslayout = new BoxLayout(betsPanel,BoxLayout.X_AXIS);
     	betsPanel.setLayout(betslayout);
-    	betsPanel.setPreferredSize(new Dimension(730, 25));
-    	betsPanel.setMaximumSize(new Dimension(730, 25)); 
-    	betsPanel.setMinimumSize(new Dimension(730, 25));
+    	betsPanel.setPreferredSize(new Dimension(900, 25));
+    	betsPanel.setMaximumSize(new Dimension(900, 25)); 
+    	betsPanel.setMinimumSize(new Dimension(900, 25));
     	winningsLabel.add(betsPanel);
     	
     	for(int i = -1; i < 5; i++){
     		JLabel betl = new JLabel((i == -1)? "BET": String.valueOf(i + 1));
     		betl.setFont(betl.getFont().deriveFont(Font.BOLD));
-    		betl.setPreferredSize(new Dimension( (i == -1)? 180: 110, 30));
-    		betl.setMaximumSize(new Dimension((i == -1)? 180: 110, 30)); 
-    		betl.setMinimumSize(new Dimension((i == -1)? 180: 110, 30));
+    		betl.setPreferredSize(new Dimension( (i == -1)? 140:140, 30));
+    		betl.setMaximumSize(new Dimension((i == -1)? 140: 140, 30)); 
+    		betl.setMinimumSize(new Dimension((i == -1)? 140: 140, 30));
     		betl.setOpaque(true);
     		betsPanel.add(betl);
     	}
@@ -115,9 +158,9 @@ public class GraphicUI extends JFrame implements UserInterface{
 	    	JPanel handPrizesLabel = new JPanel();
 	    	BoxLayout handPrizesLayout = new BoxLayout(handPrizesLabel,BoxLayout.X_AXIS);
 	    	handPrizesLabel.setLayout(handPrizesLayout);
-	    	handPrizesLabel.setPreferredSize(new Dimension(730, 25));
-	    	handPrizesLabel.setMaximumSize(new Dimension(730, 25)); 
-	    	handPrizesLabel.setMinimumSize(new Dimension(730, 25));
+	    	handPrizesLabel.setPreferredSize(new Dimension(900, 25));
+	    	handPrizesLabel.setMaximumSize(new Dimension(900, 25)); 
+	    	handPrizesLabel.setMinimumSize(new Dimension(900, 25));
 	    	
 	    	handPrizesLabel.setBackground((it % 2 == 0)? Color.WHITE : Color.LIGHT_GRAY);
 	    	
@@ -126,18 +169,18 @@ public class GraphicUI extends JFrame implements UserInterface{
 	    	JLabel handPowerLabel = new JLabel(s);
 	    	handPowerLabel.setOpaque(false);
 	    	handPowerLabel.setForeground(new Color(107,125,148));
-	    	handPowerLabel.setPreferredSize(new Dimension(180, 30));
-	    	handPowerLabel.setMaximumSize(new Dimension(180, 30)); 
-	    	handPowerLabel.setMinimumSize(new Dimension(180, 30));
+	    	handPowerLabel.setPreferredSize(new Dimension(140, 30));
+	    	handPowerLabel.setMaximumSize(new Dimension(140, 30)); 
+	    	handPowerLabel.setMinimumSize(new Dimension(140, 30));
 	    	handPrizesLabel.add(handPowerLabel);		
 	    	
 	    	int[] retArray = mGame.getWinningPrizes().getPrizeArray(s);
 	    	winningsTable[it] = new JLabel[retArray.length];
 	    	for(int it2 = 0; it2 < retArray.length ; it2++){
 	    		winningsTable[it][it2] = new JLabel(String.valueOf(retArray[it2]));
-	    		winningsTable[it][it2].setPreferredSize(new Dimension(110, 30));
-	    		winningsTable[it][it2].setMaximumSize(new Dimension(110, 30)); 
-	    		winningsTable[it][it2].setMinimumSize(new Dimension(110, 30));
+	    		winningsTable[it][it2].setPreferredSize(new Dimension(140, 30));
+	    		winningsTable[it][it2].setMaximumSize(new Dimension(140, 30)); 
+	    		winningsTable[it][it2].setMinimumSize(new Dimension(140, 30));
 	    		winningsTable[it][it2].setOpaque(true);
 	    		if(it2 == mBet - 1){
 		    		winningsTable[it][it2].setBackground((it % 2 == 0) ? 
@@ -153,9 +196,9 @@ public class GraphicUI extends JFrame implements UserInterface{
 	    //Messages Panel
 	    messagesPanel = new JPanel(new FlowLayout());
 	    messagesPanel.setVisible(true);
-	    messagesPanel.setPreferredSize(new Dimension(800, 100));
-	    messagesPanel.setMaximumSize(new Dimension(800, 100)); 
-	    messagesPanel.setMinimumSize(new Dimension(800, 100));
+	    messagesPanel.setPreferredSize(new Dimension(910, 700));
+	    messagesPanel.setMaximumSize(new Dimension(910, 700)); 
+	    messagesPanel.setMinimumSize(new Dimension(910, 700));
 	    messagesPanel.setBackground(new Color(0,0,0,0));
 	    add(messagesPanel,BorderLayout.WEST);	 
 	    
@@ -194,16 +237,16 @@ public class GraphicUI extends JFrame implements UserInterface{
 	    add(holdBox,BorderLayout.CENTER);
 	    
 	    Image spot = imageReader("cards/spot.png");
-	    Image spotResized = spot.getScaledInstance(140, 197, Image.SCALE_DEFAULT);
+	    Image spotResized = spot.getScaledInstance(160, 225, Image.SCALE_DEFAULT);
 	    
 	    CardLayout CardsOuterLayout = new CardLayout();
 	    for(int i = 0; i < 5; i++){
 	    	final int a = i;
 		    cardsOuterpannel[i] = new JPanel( CardsOuterLayout );
 		    cardsOuterpannel[i].setOpaque(false);
-	    	cardsOuterpannel[i].setPreferredSize(new Dimension(140, 197));
-	    	cardsOuterpannel[i].setMaximumSize(new Dimension(140, 197)); 
-	    	cardsOuterpannel[i].setMinimumSize(new Dimension(140, 197));
+	    	cardsOuterpannel[i].setPreferredSize(new Dimension(160, 225));
+	    	cardsOuterpannel[i].setMaximumSize(new Dimension(160, 225)); 
+	    	cardsOuterpannel[i].setMinimumSize(new Dimension(160, 225));
 	    	cardsBox.add(cardsOuterpannel[i]);
 
 	    	cardsOuterpannel[i].add( new JPanel(){
@@ -221,6 +264,16 @@ public class GraphicUI extends JFrame implements UserInterface{
 	    buttonsPanel.setLayout(buttonsLayout);
 	    buttonsPanel.setOpaque(false);
 	    cardsBox.add(buttonsPanel);
+	    
+	    //Players credit
+	    pCredit = new JLabel();
+	    pCredit.setPreferredSize(new Dimension(160, 50));
+	    pCredit.setMaximumSize(new Dimension(160, 50)); 
+	    pCredit.setMinimumSize(new Dimension(160, 50));
+	    Font font = pCredit.getFont();
+	    Font boldFont = new Font(font.getFontName(), Font.BOLD, 15);
+	    pCredit.setFont(boldFont);
+	    buttonsPanel.add(pCredit);
 	    
 	    // Bet button  
 	    betButton = new JButton("Bet");
@@ -253,6 +306,7 @@ public class GraphicUI extends JFrame implements UserInterface{
 							adviceButton.setEnabled(true);
 							dealButton.setEnabled(false);
 							drawButton.setEnabled(true);
+							displayCredit(mGame.getPlayer().getCredit());
 							displayHand(mGame.getPlayer().getHand().getHandStrArr());
 						}
 						@Override
@@ -286,6 +340,7 @@ public class GraphicUI extends JFrame implements UserInterface{
 					adviceButton.setEnabled(false);
 					drawButton.setEnabled(false);
 					displayHand(mGame.getPlayer().getHand().getHandStrArr());
+					displayCredit(mGame.getPlayer().getCredit());
 					displayResult(mGame.getWinStatus(), mGame.getWinningPrizes().
 							getHandPower(mGame.getPlayer().getHand()),
 							mGame.getPlayer().getCredit() );
@@ -338,22 +393,7 @@ public class GraphicUI extends JFrame implements UserInterface{
 	    });
 	    statsButton.setEnabled(true);
 	    buttonsPanel.add(statsButton);
-	}
-	
-	//JUST FOR DEBUGING
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					GraphicUI frame = new GraphicUI(new Game(1000));
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-		
-		//Test 
-		
+	    setResizable(false);
 	}
 	
 	private Image imageReader(String filepath){
@@ -378,8 +418,7 @@ public class GraphicUI extends JFrame implements UserInterface{
 	}
 	@Override
 	public void displayCredit(int credit) {
-		// TODO Auto-generated method stub
-		
+		pCredit.setText("Credit " + credit);
 	}
 
 	@Override
@@ -410,7 +449,7 @@ public class GraphicUI extends JFrame implements UserInterface{
 			JPanel cardsPanel = new JPanel();
 			cardsPanel.setOpaque(false);
 	    	Image cardImg = imageReader("cards/" + hand[i] + ".png");
-	    	Image cardResized = cardImg.getScaledInstance(140, 197, Image.SCALE_DEFAULT);
+	    	Image cardResized = cardImg.getScaledInstance(160, 225, Image.SCALE_DEFAULT);
 	    	JPanel innerPanel = new JPanel(new BorderLayout()){
 		        @Override public void paintComponent(Graphics g) {
 		            g.drawImage(cardResized, 0, 0, null);
@@ -528,6 +567,6 @@ public class GraphicUI extends JFrame implements UserInterface{
 	
 	@Override
 	public void run() {
-		setVisible(true);
+		creditWindow.setVisible(true);
 	}
 }
